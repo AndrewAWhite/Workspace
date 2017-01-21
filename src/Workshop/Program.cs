ced0c728be0ebea3c37f50507e52ea956ce7b2d8
Andrew White <andrew.white@ampereanalysis.com>
2017-01-21 16:34:52 +0000

    Started repository

---
+++ src/Workshop/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Workshop.GA;
+
+namespace Workshop
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine(GA.GA.Name);
+            Finished();
+        }
+
+        public static void Finished()
+        {
+            Console.WriteLine("Complete.");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ src/Workshop/Properties/AssemblyInfo.cs
@@ -0,0 +1,19 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Workshop")]
+[assembly: AssemblyTrademark("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("4f56cf36-53de-4843-af09-1a7608747894")]
